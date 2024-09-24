150e9c150dd5d2917cf95558f783f3fce3916336
Nikita Alexeev 302 <nikitalexxx1@gmail.com>
2024-09-24 11:38:17 +0300

    some mistakes was deleted

--- src/test/java/ru/alexeev/restfulvkusers/ServiceTests.java
+++ src/test/java/ru/alexeev/restfulvkusers/ServiceTests.java
@@ -61,6 +61,7 @@ public class ServiceTests {
                 .andExpect((jsonPath("$.firstName").value("Nikita")))// Проверяем поле "firstName"
                 .andExpect(jsonPath("$.member").value(true)); // Проверяем поле "isMember"
     }
+
     @Test
     public void shouldReturnBadRequest_whenInvalidRequest() throws Exception {
         // Создаем некорректный объект запроса
@@ -76,58 +77,5 @@ public class ServiceTests {
                         .content(requestJson))
                 .andExpect(status().isBadRequest()); // Проверяем, что сервер вернет статус 400
     }
-
-//    @Test
-//    public void testPostMethod() {
-//        String jsonBody = "{\"user_id\":269866241,\"group_id\":168131127}";
-//        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
-//        headers.put("Content-Type", List.of(MediaType.APPLICATION_JSON));
-//        headers.put("vk_access_token", List.of(token));
-//
-//        Response response = ClientBuilder.newClient().target("http://localhost:8080/api/v1/user-info")
-//                .request(String.valueOf(MediaType.APPLICATION_JSON))
-//                .headers(headers)
-//                .post(Entity.json(jsonBody));
-//
-//        assertEquals(200, response.getStatus());
-//        assertNotNull(response.readEntity(String.class));
-//    }
 }
-
-//    @Test
-//    public void testGetFirstName() throws IOException {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setBearerAuth("vk_service_token");
-//        Mono<String> responseBody = webTestClient.get()
-//                .uri("/v1/users/269866241")
-//                .headers(h -> h.putAll(headers))
-//                .accept(MediaType.APPLICATION_JSON).exchange()
-//                .flatMap(response -> response.bodyToMono(String.class));
-//        assertEquals("Никита", responseBody.block());
-//    }
-//
-//    @Test
-//    public void testIsMember() throws IOException {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setBearerAuth("vk_service_token");
-//        Mono<Boolean> responseBody = webTestClient.get().uri("https://api.vk.com/method/groups.isMember?access_token=d7535714d7535714d7535714f7d44d9307dd753d7535714b1a3d395ee76c8f468fceb3e&group_id=168131127&user_id=269866241&v=5.199")
-//                .headers(h -> h.putAll(headers))
-//                .accept(MediaType.APPLICATION_JSON).exchange()
-//                .flatMap(response -> response.bodyToMono(Boolean.class));
-//        assertTrue(responseBody.block());
-//    }
-//
-//    @Test
-//    public void testFullNameResponse() throws IOException {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setBearerAuth("vk_service_token");
-//        String fullNameJson = "{\"user_id\":\"18391\", \"group_id\":\"30131\"}";
-//        Mono<String> responseBody = webTestClient.post().uri("/v1/full_name")
-//                .headers(h -> h.putAll(headers))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .syncBody(fullNameJson)
-//                .exchange()
-//                .flatMap(response -> response.bodyToMono(String.class));
-//        assertEquals("{\"first_name\":\"Никита\",\"is_member\":true}", responseBody.block());
-//    }
 
